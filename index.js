f5740b02535ac038be038fafe2be189df489f112
Kaspar <kaspar.viitkar@voco.ee>
2024-10-10 15:29:26 +0300

    Write data to file

--- index.js
+++ index.js
@@ -3,21 +3,47 @@ const app = express()
 const fs = require('fs');
 
 const path = require('path')
+
+app.use(express.urlencoded({ extended: true} ));
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if(err) {
+                console.error(err);
+                return;
+            }
+            const tasks = data.split('\n')
+            resolve(tasks)
+        });
+    })
+}
+
 app.get('/', (req, res) => {
-    fs.readFile('./tasks', 'utf8', (err, data) => {
-        if(err) {
-            console.error(err);
-            return;
-        }
-        
-        const tasks = data.split('\n')
-        res.render('index', {tasks: tasks})
-    });
-    /* const tasks = ['Study HTML', 'Study CSS', 'Study JS', 'Study Node.js']
-    res.render('index', {tasks: tasks}) */
+    readFile('./tasks')
+        .then(tasks => { 
+             res.render('index', {tasks: tasks})
+    })
+})
+
+app.post('/', (req, res) => {
+    console.log('form sent data')
+    let task = req.body.task
+    readFile('./tasks')
+        .then((tasks) => {
+            tasks.push(task)
+            const data = tasks.join('\n')
+            fs.writeFile('./tasks', data, err => {
+                if(err) {
+                    console.error(err);
+                } else {
+                    res.redirect('/')
+                }
+        });    
+    })
 })
 
 app.listen(3001, () => {
