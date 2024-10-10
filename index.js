d365de1c5038d474647f966c8ffeae38cf5c9fac
Kaspar <kaspar.viitkar@voco.ee>
2024-10-10 13:53:47 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
-
 const app = express()
+const fs = require('fs');
 
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    fs.readFile('./tasks', 'utf8', (err, data) => {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        
+        const tasks = data.split('\n')
+        res.render('index', {tasks: tasks})
+    });
+    /* const tasks = ['Study HTML', 'Study CSS', 'Study JS', 'Study Node.js']
+    res.render('index', {tasks: tasks}) */
 })
 
 app.listen(3001, () => {
