81b9ec89dc858322919187867e286ffb17d2efb4
Kaspar <kylgorvigajava@gmail.com>
2024-11-05 12:47:36 +0200

    Add error handling

--- index.js
+++ index.js
@@ -37,30 +37,44 @@ const writeFile = (filename, date) => {
 app.get('/', (req, res) => {
     readFile('./tasks.json')
         .then(tasks => { 
-            console.log(tasks)
-            res.render('index', {tasks: tasks})
+            res.render('index', {
+                tasks: tasks,
+                error: null
+        }) 
     })
 })
 
 app.post('/', (req, res) => {
+    let error = null
+    if(req.body.task.trim().length == 0) {
+    error = 'Insert correct task data'
+    console.log(error)
     readFile('./tasks.json')
         .then(tasks => {
-            let index
-            if(tasks.length === 0) {
-                index = 0
-            } else {
-                index = tasks[tasks.length - 1].id +1;
-            }
-            const newTask = {
-                "id" : index,
-                "task" : req.body.task
-            }
-            tasks.push(newTask)
-            data = JSON.stringify(tasks, null, 2)
-            console.log(data)
-            writeFile('./tasks.json', data)
-            res.redirect('/')
+            res.render('index', {
+                tasks: tasks, 
+                error: error
+        })
     })
+} else {
+    readFile('./tasks.json')
+    .then(tasks => {
+    let index
+        if(tasks.length === 0) {
+            index = 0
+        } else {
+            index = tasks[tasks.length - 1].id +1;
+        }
+        const newTask = {
+            "id" : index,
+            "task" : req.body.task
+        }
+        tasks.push(newTask)
+        data = JSON.stringify(tasks, null, 2)
+        writeFile('./tasks.json', data)
+        res.redirect('/')
+        })
+    }
 })
 
 app.get('/delete-task/:taskId', (req, res) => {
